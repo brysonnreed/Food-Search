770c1ffcbc1bad4aa1300162226b3a58a4884cc3
Bryson Reed <brysonjreed@gmail.com>
2024-08-03 20:23:08 -0500

    Add longer timeout on not found search

--- src/components/UnknownSearch.vue
+++ src/components/UnknownSearch.vue
@@ -1,34 +1,34 @@
 <template>
-  <div
-    v-if="searchPerformed && !meals.length && shouldShow == true"
-    class="flex flex-col justify-center items-center text-gray-600 min-h-[60vh]"
-  >
-    <h2 class="text-[100px] text-orange-500 font-bold text-center">Oops</h2>
-    <p class="text-2xl text-gray-500 mb-10 text-center">
-      We couldn't find any meals with that Search.
-    </p>
-    <router-link
-      to="/"
-      class="px-3 py-2 rounded text-white bg-orange-500 hover:bg-orange-600 border-2 border-orange-500 hover:border-orange-600 transition-colors duration-200"
-      >Go back home</router-link
-    >
-    <img src="/Cartoon-fast-food.png" alt="Food Illustration" />
-  </div>
+	<div
+		v-if="searchPerformed && !meals.length && shouldShow == true"
+		class="flex flex-col justify-center items-center text-gray-600 min-h-[60vh]"
+	>
+		<h2 class="text-[100px] text-orange-500 font-bold text-center">Oops</h2>
+		<p class="text-2xl text-gray-500 mb-10 text-center">
+			We couldn't find any meals with that Search.
+		</p>
+		<router-link
+			to="/"
+			class="px-3 py-2 rounded text-white bg-orange-500 hover:bg-orange-600 border-2 border-orange-500 hover:border-orange-600 transition-colors duration-200"
+			>Go back home</router-link
+		>
+		<img src="/Cartoon-fast-food.png" alt="Food Illustration" />
+	</div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+	import { onMounted, ref } from "vue";
 
-const { meals } = defineProps({
-  meals: Object,
-  searchPerformed: Boolean,
-});
+	const { meals } = defineProps({
+		meals: Object,
+		searchPerformed: Boolean,
+	});
 
-const shouldShow = ref(false);
+	const shouldShow = ref(false);
 
-onMounted(() => {
-  setTimeout(() => {
-    shouldShow.value = true;
-  }, 1000);
-});
+	onMounted(() => {
+		setTimeout(() => {
+			shouldShow.value = true;
+		}, 2000);
+	});
 </script>
